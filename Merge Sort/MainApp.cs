ba9e9676f3e831498d6bf4fcfe7fe3bf9f8dc5c3
jei-W <gkdtkdv123@naver.com>
2019-02-24 11:19:38 +0900

    병합정렬의 구현 == *오름차순 정렬 구현 *내림차순 정렬 구현

---
+++ Merge Sort/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    class MainApp
+    {
+        static void Main( string[] args )
+        {
+            int[] arg = { 1, 2, 7, 16, 9, 3, 8, 2, 30, 24 };
+            Print();
+
+            MergeSort.AscendingSort(ref arg);
+            Print();
+
+            MergeSort.DescendingSort(ref arg);
+            Print();
+
+            void Print()
+            {
+                foreach( int i in arg )
+                {
+                    Console.Write($"{i} ");
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Merge Sort/MergeSort.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    class MergeSort
+    {
+        public static void AscendingSort( ref int[] arg )
+        {
+            int[][] half = Split(arg);
+
+
+            for( int i = 0; i < 2; i++ )
+            {
+                if( half[i].Length > 1 )
+                {
+                    AscendingSort(ref half[i]);
+                }
+            }
+
+            arg = MergeForAscendingSort(half[0], half[1]);
+            
+
+            int[] MergeForAscendingSort( int[] a, int[] b )
+            {
+                int[] result = new int[a.Length + b.Length];
+                int aIndex = 0;
+                int bIndex = 0;
+
+                for( int i = 0; i < result.Length; i++ )
+                {
+                    if( aIndex >= a.Length )
+                    {
+                        result[i] = b[bIndex];
+                        bIndex++;
+                    }
+
+                    else if( bIndex >= b.Length )
+                    {
+                        result[i] = a[aIndex];
+                        aIndex++;
+                    }
+
+                    else if( a[aIndex] <= b[bIndex] )
+                    {
+                        result[i] = a[aIndex];
+                        aIndex++;
+                    }
+
+                    else if( a[aIndex] > b[bIndex] )
+                    {
+                        result[i] = b[bIndex];
+                        bIndex++;
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public static void DescendingSort( ref int[] arg )
+        {
+            int[][] half = Split(arg);
+
+
+            for( int i = 0; i < 2; i++ )
+            {
+                if( half[i].Length > 1 )
+                {
+                    DescendingSort(ref half[i]);
+                }
+            }
+
+            arg = MergeForDescendingSort(half[0], half[1]);
+
+
+            int[] MergeForDescendingSort( int[] a, int[] b )
+            {
+                int[] result = new int[a.Length + b.Length];
+                int aIndex = 0;
+                int bIndex = 0;
+
+                for( int i = 0; i < result.Length; i++ )
+                {
+                    if( aIndex >= a.Length )
+                    {
+                        result[i] = b[bIndex];
+                        bIndex++;
+                    }
+
+                    else if( bIndex >= b.Length )
+                    {
+                        result[i] = a[aIndex];
+                        aIndex++;
+                    }
+
+                    else if( a[aIndex] >= b[bIndex] )
+                    {
+                        result[i] = a[aIndex];
+                        aIndex++;
+                    }
+
+                    else if( a[aIndex] < b[bIndex] )
+                    {
+                        result[i] = b[bIndex];
+                        bIndex++;
+                    }
+                }
+
+                return result;
+            }
+        }
+
+
+
+
+        static int[][] Split( int[] arg )
+        {
+            int ForwardHarfSize = arg.Length / 2;
+            int BackwardHalfSizs = arg.Length % 2 == 1 ? ForwardHarfSize + 1 : ForwardHarfSize;
+
+            int[][] result = { new int[ForwardHarfSize], new int[BackwardHalfSizs] };
+
+            for( int i = 0; i < ForwardHarfSize; i++ )
+            {
+                result[0][i] = arg[i];
+            }
+          
+            for( int j = 0; j < BackwardHalfSizs; j++ )
+            {
+                result[1][j] = arg[ForwardHarfSize + j];
+            }
+
+            return result;
+        }
+    }
+}
